9d43a7b5da5226d2a210119f6a0979db964da9b0
JackyHuth <137807502+JackyHuth@users.noreply.github.com>
2023-07-23 23:13:38 +0200

    folder management

--- Trinket's Tower Defense/Assets/Prefabs/Enemies/BasePrefab/EnemyBase.cs
+++ Trinket's Tower Defense/Assets/Prefabs/Enemies/BasePrefab/EnemyBase.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MonsterBase : EntityBase
+public class EnemyBase : EntityBase
 {   
     [Header("Stats")]
     [SerializeField] private int health;

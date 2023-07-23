6d6c0fd0b87373537d655cf681268e252c89cae1
LamiiaSuu <140017712+LamiiaSuu@users.noreply.github.com>
2023-07-23 22:11:02 +0200

    Enemy Prefab and main fix

---
+++ Trinket's Tower Defense/Assets/EnemySpawner.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+
+    [Header("References")]
+    [SerializeField] private GameObject[] enemyPrefabs;
+
+
+    [Header("Attributes")]
+    [SerializeField] private int baseEnemies = 8;
+    [SerializeField] private float enemiesPerSecond = 0.5f;
+    [SerializeField] private float timeBetweenWaves = 5f;
+
+
+
+    private int currentWave = 1;
+    private float timeSinceLastSpawn;
+    private int enemiesAlive;
+    private int enemiesLeftToSpawn;
+    private bool isSpawning = false;
+
+    private void Start() {
+        StartWave();
+    }
+
+    private void Update(){
+        if (!isSpawning)
+        return;
+
+        timeSinceLastSpawn += Time.deltaTime;
+
+        if(timeSinceLastSpawn >= (1f / enemiesPerSecond)){
+            Debug.Log("Spawn Enemy");
+            timeSinceLastSpawn = 0f;
+        }
+    }
+
+    private void StartWave() {
+        isSpawning = true;
+        enemiesLeftToSpawn = baseEnemies;
+    }
+
+    private GameObject[] getRound(int roundNumber) {
+        
+        return StandardRounds.getRoundEnemies;
+    }
+}

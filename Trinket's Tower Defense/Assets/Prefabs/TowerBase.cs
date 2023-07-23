d8cfba5d710128340578992085477a3ecc8ead1b
JackyHuth <137807502+JackyHuth@users.noreply.github.com>
2023-07-23 20:59:47 +0200

    Added folders & saved tower prefab

--- Trinket's Tower Defense/Assets/Prefabs/TowerBase.cs
+++ Trinket's Tower Defense/Assets/Prefabs/TowerBase.cs
@@ -1,12 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static Stats;
+using static DamageTypes;
 
-public class Tower_Base : MonoBehaviour
+public class TowerBase : MonoBehaviour
 {
+    enum TowerLevel
+    {
+        Level1,
+        Level2,
+        Level3,
+        Level4,
+        Level5
+    }
+
     [Header("Cost")]
     [SerializeField] private int baseCost;
     [SerializeField] private int firstUpgradeCost;
+
+    [Header("Status")]
+    [SerializeField] private TowerLevel currentTowerLevel;
 
     // Start is called before the first frame update
     void Start()
